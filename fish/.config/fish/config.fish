1776b8d4730643cf1e435350f16ab351167fc4dd
Carlisia Thompson <carlisia@grokkingtech.io>
2022-02-18 20:59:34 -0800

    Fix shellhistory

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -122,6 +122,21 @@ if [ ! -n "$__shhist_session" ]
     end
 end
 
+functions --copy fish_prompt fish_prompt_original
+function fish_prompt
+    set __shhist_status $status
+
+    if fish_is_root_user
+        set __shhist_user $SUDO_USER
+    else
+        set __shhist_user $LOGNAME
+    end
+
+    \history --show-time="%s " -1 | sudo --preserve-env --user $__shhist_user /Applications/ShellHistory.app/Contents/Helpers/shhist insert --session $__shhist_session --username $LOGNAME --hostname (hostname) --exit-code $__shhist_status --shell fish
+
+    fish_prompt_original;
+end
+
 
 [ -f /usr/local/share/autojump/autojump.fish ];
 source /usr/local/share/autojump/autojump.fish

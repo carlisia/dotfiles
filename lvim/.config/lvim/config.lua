410011e1bbe8c2e977c2452b959c613cbbc44396
Carlisia Thompson <carlisia@grokkingtech.io>
2022-07-14 17:20:13 -0700

    Refresh lvim, plain-ish

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,19 +1,24 @@
---[[ Notes: {{{1
-Useful Links and TODO
-http://cheat.sh
-Buffer bar info: https://github.com/romgrk/barbar.nvim
--- }}}1 ]]
+--[[
+lvim is the global options object
 
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
--- Settings {{{1
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.auto_complete = true
-lvim.termguicolors = true
+lvim.colorscheme = "onedarker"
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -22,9 +27,7 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
-
--- LANGUAGES -----------------------------------
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -33,6 +36,7 @@ lvim.builtin.treesitter.ensure_installed = {
   "go",
   "json",
   "lua",
+  "markdown",
   "python",
   "typescript",
   "yaml",
@@ -40,28 +44,13 @@ lvim.builtin.treesitter.ensure_installed = {
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
-lvim.builtin.treesitter.context_commentstring.enable = true
-lvim.builtin.treesitter.context_commentstring.enable_autocmd = true
 
+-- generic LSP settings
 
-vim.diagnostic.config({ virtual_text = false })
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.automatic_servers_installation = false
 
-lvim.builtin.gitsigns.opts.signs.add.text = ''
-lvim.builtin.gitsigns.opts.signs.change.text = ''
-lvim.builtin.gitsigns.opts.signs.delete.text = ''
-lvim.builtin.gitsigns.opts.signs.topdelete.text = ''
-lvim.builtin.gitsigns.opts.signs.changedelete.text = ''
-
-lvim.lsp.diagnostics.signs.values = {
-  { name = "LspDiagnosticsSignError", text = "" },
-  { name = "LspDiagnosticsSignWarning", text = "" },
-  { name = "LspDiagnosticsSignHint", text = '' },
-  { name = "LspDiagnosticsSignInformation", text = "" },
-}
-
---}}}
-
--- Dashboard/Alpha {{{1
+-- {{{ Dashboard
 
 lvim.builtin.alpha.dashboard.section.header.opts.hl = ""
 -- Shorter ASCII art logo, so not too much space is taken up.
@@ -77,11 +66,22 @@ lvim.builtin.alpha.dashboard.section.buttons.entries = {
   { "SPC n", "  New File", "<CMD>ene!<CR>" },
   { "SPC p", "  Recent Projects ", "<CMD>Telescope projects<CR>" },
   { "SPC u", "  Recently Used Files", "<CMD>Telescope oldfiles<CR>" },
-  { "SPC s", "  Load last session", "<CMD>SessionLoad<CR>" },
-  { "SPC r", "/  Ranger", "<CMD>RnvimrToggle<CR>" },
+  -- { "SPC s",   "  Load last session",           "<CMD>SessionLoad<CR>" },
+  { "SPC r", "  Ranger", "<CMD>RnvimrToggle<CR>" },
   { "SPC m", "  Marks              ", "<CMD>Telescope marks<CR>" },
   { "SPC w", "  Find Word", "<CMD>Telescope live_grep<CR>" },
-  { "SPC c", "  Edit Configuration", "<CMD>e ~/.config/lvim/config.lua<CR>" },
+  { "SPC c", "  Edit Configuration", "<CMD>e ~/bin/config/configFiles.md<CR>" },
   { "SPC g", "  Git status", "<CMD>Telescope git_status<CR>" }
 }
 --}}}
+
+
+
+-- Additional Plugins
+-- lvim.plugins = {
+--     {"folke/tokyonight.nvim"},
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+-- }

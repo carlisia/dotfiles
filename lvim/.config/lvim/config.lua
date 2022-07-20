364e6fcba95b53790d30b85c2f9f196eb053853f
Carlisia Thompson <carlisia@grokkingtech.io>
2022-07-19 17:45:55 -0700

    Fix indentation configs

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -188,19 +188,13 @@ lvim.plugins = {
   {
     "lukas-reineke/indent-blankline.nvim",
     event = "BufRead",
-    setup = function()
-      vim.g.indentLine_enabled = 1
-      vim.g.indent_blankline_char = "▏"
-      vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
-      vim.g.indent_blankline_buftype_exclude = { "terminal" }
-      vim.g.indent_blankline_show_trailing_blankline_indent = false
-      vim.g.indent_blankline_show_first_indent_level = false
-
-      vim.opt.list = true
-      vim.opt.listchars:append("space:⋅")
-      vim.opt.listchars:append("eol:↴")
-
+    config = function()
       require("indent_blankline").setup {
+        char = "▏",
+        filetype_exclude = { "help", "terminal", "dashboard", "NvimTree", "packer", "lsp-installer" },
+        buftype_exclude = { "terminal" },
+        show_trailing_blankline_indent = false,
+        show_first_indent_level = false,
         space_char_blankline = " ",
         show_current_context = true,
         show_current_context_start = true,

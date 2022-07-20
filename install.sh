08eb8a1a20bb14426da4f47677e6b5ede7307e68
Carlisia Thompson <carlisia@grokkingtech.io>
2022-07-20 13:10:33 -0700

    Update codespace installation

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
  
- if [ -z "$USER" ]; then
+if [ -z "$USER" ]; then
     USER=$(id -un)
 fi
 
 echo >&2 "====================================================================="
-echo >&2 " Setting up codespaces environment"
+echo >&2 " Setting up codespaces environment (linux based)"
 echo >&2 ""
 echo >&2 " USER        $USER"
 echo >&2 " HOME        $HOME"
@@ -21,9 +21,6 @@ rm -rf .oh-my-bash
 rm -rf .oh-my-zsh
 rm .zshrc
 
-# A bit of a hack
-mv .gitconfig .gitconfig.private
-
 # Install fzf
 FZF_VERSION=0.30.0
 curl -L https://github.com/junegunn/fzf/releases/download/"${FZF_VERSION}"/fzf-"${FZF_VERSION}"-linux_amd64.tar.gz | tar xzC "$HOME"/bin
@@ -34,7 +31,14 @@ sudo apt-get install -y libfuse2
 curl -L -o "$HOME"/bin/nvim https://github.com/neovim/neovim/releases/download/v"${NVIM_VERSION}"/nvim.appimage
 chmod a+x "$HOME"/bin/nvim
 
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# - Run  these two commands in your terminal to add Homebrew to your PATH:
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/codespace/.profile
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# - Install Homebrew's dependencies if you have sudo access:
+    sudo apt-get install build-essential
+# - We recommend that you install GCC:
+    brew install gcc
 
 brew install stow
 
@@ -46,9 +50,13 @@ stow gh -t "$HOME"
 
 stow git -t "$HOME"
 
-stow lvim -t "$HOME"
+bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
 
 stow nvim -t "$HOME"
+
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+
+stow lvim -t "$HOME"
 
 stow starfish -t "$HOME"
 

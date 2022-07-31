11a4f778703f05388b808d73381f063679664f65
Carlisia Thompson <carlisia@grokkingtech.io>
2022-07-31 12:10:52 -0700

    Delete lvim

--- lvim/.config/lvim/lua/config/color_nvcode-color-schemes.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/ChristianChiarulli/nvcode-color-schemes.vim 
-require("nvim-treesitter.configs").setup({
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "c", "rust" }, -- list of language that will be disabled
-  }
-})
-

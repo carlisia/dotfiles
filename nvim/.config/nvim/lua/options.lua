337f5af0328e85a78e6d25655c1b1c6ad0de044d
Carlisia Campos <carlisia@gmail.com>
2025-03-15 13:27:17 -0700

    Remove (very) old keybindings file

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,11 +1,8 @@
+require "nvchad.options"
+
 -- How to set options from Lua:
 -- Use vim.opt only for "list"/"map" like options ('listchars', 'fillchars', etc.) as it has :append() / :remove() methods,
 -- and use vim.o for everything else.
 -- Using vim.opt for all options is not an error, but vim.o is a more reliable interface.
-
--- Always show a global status line, where a single status line spans the entire bottom:
-local option = vim.o
-local opt = vim.opt
-
-option.laststatus = 3
-opt.clipboard:append "unnamedplus"
+-- local option = vim.o
+-- local opt = vim.opt

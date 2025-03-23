8d61cdb97c7d1e4e86c5a15df92d75eecf2f5648
Carlisia Campos <carlisia@gmail.com>
2025-03-23 13:10:33 -0700

    Clean up cmp/snippets mappings

--- nvim/.config/nvim/lua/configs/completion.lua
+++ nvim/.config/nvim/lua/configs/completion.lua
@@ -127,41 +127,6 @@ M.cmp = {
     end,
   },
 
-  --- Use TAB for snippets
-  ["<CR>"] = cmp.mapping(function(fallback)
-    if cmp.visible() then
-      if luasnip.expandable() then
-        luasnip.expand()
-      else
-        cmp.confirm {
-          select = true,
-        }
-      end
-    else
-      fallback()
-    end
-  end),
-
-  ["<Tab>"] = cmp.mapping(function(fallback)
-    if cmp.visible() then
-      cmp.select_next_item()
-    elseif luasnip.locally_jumpable(1) then
-      luasnip.jump(1)
-    else
-      fallback()
-    end
-  end, { "i", "s" }),
-
-  ["<S-Tab>"] = cmp.mapping(function(fallback)
-    if cmp.visible() then
-      cmp.select_prev_item()
-    elseif luasnip.locally_jumpable(-1) then
-      luasnip.jump(-1)
-    else
-      fallback()
-    end
-  end, { "i", "s" }),
-
   experimental = { ghost_text = true },
 
   -- Enable luasnip to handle snippet expansion for nvim-cmp

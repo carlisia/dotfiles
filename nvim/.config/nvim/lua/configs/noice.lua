69893b9a8f96903f116e2340b00556a0eaad1b5e
Carlisia Campos <carlisia@gmail.com>
2025-04-01 08:42:59 -0700

    Minior ui improvements - remove double notifications, better vi ops

--- nvim/.config/nvim/lua/configs/noice.lua
+++ nvim/.config/nvim/lua/configs/noice.lua
@@ -10,12 +10,12 @@ M.config = {
     -- This is a current Neovim limitation.
     enabled = true, -- enables the Noice messages UI
     view = "notify", -- default view for messages
-    view_error = "notify", -- view for errors
-    view_warn = "notify", -- view for warnings
+    view_error = false, -- view for errors
+    view_warn = false, -- view for warnings
     view_history = "messages", -- view for :messages
     view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
   },
-  notify = { enabled = true },
+  notify = { enabled = false },
   notifier = {
     top_down = false,
   },
--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -6,12 +6,13 @@ require "nvchad.options"
 -- Using vim.opt for all options is not an error, but vim.o is a more reliable interface.
 local option = vim.o
 local g = vim.g
+local opt = vim.opt
 
 vim.api.nvim_set_hl(0, "SnacksPickerDir", { link = "Text" })
 vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { link = "Text" })
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20" -- just in case
 
-vim.opt.termguicolors = true
+opt.termguicolors = true
 option.cursorlineopt = "both"
 g.markdown_folding = 1
 
@@ -21,3 +22,4 @@ require("luasnip.loaders.from_vscode").lazy_load {
 }
 
 g.root_spec = { "cwd" }
+opt.clipboard:append { "unnamed", "unnamedplus" }

1997cc0bd795862470ca3f8b5a26ecb05de9266a
Carlisia Campos <carlisia@gmail.com>
2025-04-06 07:41:36 -0700

    Use go.nvim lsp config + add lua lsp hints

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,34 +1,17 @@
 -- [nvim-lspconfig/doc/configs.md at master · neovim/nvim-lspconfig](https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md)
-
 local configs = require "nvchad.configs.lspconfig"
 
 local servers = {}
-local gopls = {
-  -- https://github.com/golang/tools/blob/3e76cae71578160dca62d1cab42a715ef960c892/gopls/doc/settings.md
-  -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md
-  settings = {
-    gopls = { -- overriding defaults only
-      analyses = {
-        shadow = true,
-      },
-      gofumpt = true,
-      staticcheck = true,
-      usePlaceholders = true,
-    },
-  },
-  init_options = {
-    usePlaceholders = true,
-  },
-}
 
-local sqlls = {
+-- https://github.com/golang/tools/blob/3e76cae71578160dca62d1cab42a715ef960c892/gopls/doc/settings.md
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md
+require("go").setup(require("configs.go_plugins").govim)
+servers["gopls"] = require("go.lsp").config()
+
+servers["sqlls"] = {
   filetypes = { "sql", "mysql", "plsql" },
 }
 
-servers["gopls"] = gopls
-servers["sqlls"] = sqlls
-servers["marksman"] = {}
-servers["taplo"] = {}
 servers["yamlls"] = {
   settings = {
     yaml = {
@@ -54,14 +37,27 @@ servers["jsonls"] = {
   },
 }
 
+servers["marksman"] = {}
+servers["taplo"] = {}
+
+require("lspconfig").lua_ls.setup {
+  settings = {
+    Lua = {
+      hint = {
+        enable = true,
+        paramName = "All",
+        paramType = true,
+        setType = true,
+        arrayIndex = "Auto",
+        semicolon = "Disable",
+      },
+    },
+  },
+}
+
 for name, opts in pairs(servers) do
   opts.on_init = configs.on_init
   opts.capabilities = configs.capabilities
-
-  opts.capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true,
-  }
 
   require("lspconfig")[name].setup(opts)
 end
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -182,9 +182,6 @@ return {
       "theHamsta/nvim-dap-virtual-text",
       "nvim-treesitter/nvim-treesitter",
     },
-    config = function()
-      require("go").setup(require("configs.go_plugins").govim)
-    end,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod", "gosum", "gotmpl", "gohtmltmpl", "gotexttmpl" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
